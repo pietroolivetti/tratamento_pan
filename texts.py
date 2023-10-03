6652b0819b72bdb1226d981fa372737b917a0b68
Pietro Olivetti <72519384+pietroolivetti@users.noreply.github.com>
2023-10-03 23:23:43 +0000

    added sintomas fisicos

--- texts.py
+++ texts.py
@@ -21,7 +21,7 @@ Vamos rever todos os benefícios do registro contínuo, bem como as razões pela
 - Para avaliar o sucesso de seus esforços para mudar."""],
 
 
-    'O propósito da ansiedade e do pânico': ["""A ansiedade e o pânico são estados emocionais naturais que são experimentados por todos fazem parte da experiência de ser humano. A ansiedade é a reação que todos nós temos sempre que pensamos que algo ruim ou ameaçador pode acontecer. Essas coisas ameaçadoras podem incluir ameaças físicas, como a possibilidade de doença, acidente ou morte; ameaças sociais, como a possibilidade de constrangimento, rejeição ou ridicularização; ou ameaças mentais, como a possibilidade de enlouquecer, perder o controle ou perder a faculdade mental. As ameaças podem ser grandes (como a possibilidade de perder a vida) ou pequenas (como a possibilidade de se atrasar para um compromisso). A ansiedade é nossa maneira de nos prepararmos para enfrentar o desafio apresentado por essas ameaças. Ela nos ajuda a nos preparar e nos proteger do que está ameaçando.
+    'O propósito da ansiedade e do pânico': ["""A ansiedade e o pânico são estados emocionais naturais que são experimentados por todos que fazem parte da experiência de ser humano. A ansiedade é a reação que todos nós temos sempre que pensamos que algo ruim ou ameaçador pode acontecer. Essas coisas ameaçadoras podem incluir ameaças físicas, como a possibilidade de doença, acidente ou morte; ameaças sociais, como a possibilidade de constrangimento, rejeição ou ridicularização; ou ameaças mentais, como a possibilidade de enlouquecer, perder o controle ou perder a faculdade mental. As ameaças podem ser grandes (como a possibilidade de perder a vida) ou pequenas (como a possibilidade de se atrasar para um compromisso). A ansiedade é nossa maneira de nos prepararmos para enfrentar o desafio apresentado por essas ameaças. Ela nos ajuda a nos preparar e nos proteger do que está ameaçando.
 
 A ansiedade não é má em si mesma e pode ser uma força produtiva e motivadora. Anos de pesquisa mostraram que ter alguma ansiedade melhora o desempenho. Ou seja, você se sai melhor no que está fazendo, seja na sala de aula ou no trabalho, quando a ansiedade está presente até certo nível. Imagine não ter absolutamente nenhuma ansiedade em uma entrevista de emprego - você estaria menos motivado para mostrar o seu melhor e enfrentar o desafio de conseguir o emprego. Alguns psicólogos chamaram a ansiedade de "a sombra da inteligência" porque nos motiva a pensar adiante e nos preparar para os desafios que podemos enfrentar. No entanto, a ansiedade pode variar em gravidade, desde um leve desconforto até uma angústia extrema. No extremo, a ansiedade pode interferir no que estamos fazendo. Imagine uma ansiedade extrema em uma entrevista de emprego que leva a uma concentração ruim e gagueira. Alguma ansiedade é útil para o aprendizado e o desempenho, enquanto muito ansiedade pode interferir negativamente nos mesmos processos.  
 
@@ -41,12 +41,63 @@ Comportamentos: São as coisas que fazemos, como andar de um lado para o outro, 
 Esses três componentes frequentemente diferem nos momentos em que você está ansioso em relação aos momentos em que está em pânico. Os pensamentos durante a ansiedade geralmente se relacionam com o futuro (por exemplo, "Meu chefe pode me dar uma avaliação negativa no final do ano" ou "Seria horrível se eu entrasse em pânico na festa de amanhã"), enquanto os pensamentos durante ataques de pânico geralmente se referem à situação imediata (por exemplo, "Vou desmaiar ou morrer agora" ou "Estou enlouquecendo"). Além disso, os comportamentos ansiosos incluem evitar situações ou aumentar a cautela (como planejar totalmente as direções com antecedência para evitar se perder), enquanto os comportamentos durante o pânico têm mais a ver com escapar ou procurar ajuda. Finalmente, os sintomas físicos durante a ansiedade geralmente são de longa duração e envolvem tensão muscular, inquietação e fadiga; em contraste, os sintomas dos ataques de pânico são mais abruptos e tendem a diminuir mais rapidamente do que os sintomas físicos da ansiedade, e incluem palpitações cardíacas, falta de ar e outros sintomas.
 
 """, ['Formulário 3.2: Componentes da Ansiedade','https://drive.google.com/file/d/1c68H6yTWv_DkXAeh6j6C0j9-K8hugJn-/view?usp=drive_link']],
-    'O ciclo do pânico': ["""Os sintomas físicos, pensamentos e comportamentos contribuem uns para os outros em um que é chamado de ciclo negativo. Em outras palavras, eles tendem a se alimentar mutuamente. Por exemplo, pensamentos negativos podem aumentar diretamente os sintomas físicos. Se nos dizemos que algo perigoso está prestes a acontecer (por exemplo, "Estou prestes a ter um ataque cardíaco"), então a tensão física aumentará porque nossos corpos liberam mais adrenalina e operam em ritmo mais rápido sempre que enfrentamos perigo. Por sua vez, um sintoma físico, como um coração acelerado, pode levar a mais pensamentos negativos. Isso é particularmente provável se você acreditar que os sintomas normais de tensão são perigosos (por exemplo, "O fato de que minha frequência cardíaca não diminuiu deve significar que algo está terrivelmente errado"). Comportamentos como agitação, caminhar de um lado para o outro e escapar de uma situação também podem aumentar os níveis de tensão física devido ao esforço físico envolvido.
+    'O ciclo do pânico': ["""Os sintomas físicos, pensamentos e comportamentos contribuem uns para os outros no que é chamado de ciclo negativo. Em outras palavras, eles tendem a se alimentar mutuamente. Por exemplo, pensamentos negativos podem aumentar diretamente os sintomas físicos. Se nos dizemos que algo perigoso está prestes a acontecer (por exemplo, "Estou prestes a ter um ataque cardíaco"), então a tensão física aumentará porque nossos corpos liberam mais adrenalina e operam em ritmo mais rápido sempre que enfrentamos perigo. Por sua vez, um sintoma físico, como um coração acelerado, pode levar a mais pensamentos negativos. Isso é particularmente provável se você acreditar que os sintomas normais de tensão são perigosos (por exemplo, "O fato de que minha frequência cardíaca não diminuiu deve significar que algo está terrivelmente errado"). Comportamentos como agitação, caminhar de um lado para o outro e escapar de uma situação também podem aumentar os níveis de tensão física devido ao esforço físico envolvido.
 
 Mais especificamente, o pensamento de que um coração acelerado é um sinal de doença cardíaca é assustador e produzirá uma frequência cardíaca ainda mais rápida. Por sua vez, uma frequência cardíaca mais rápida pode levar a crenças mais fortes de que algo está terrivelmente errado com o coração. Isso também pode levar a tentativas de obter ajuda médica. Tais pensamentos e comportamentos negativos podem novamente prolongar a aceleração do coração. Em outras palavras, pensamentos negativos levam ao medo, e o medo leva a mais sintomas físicos e comportamentos de fuga que se acumulam em mais pensamentos negativos, e assim por diante. O resultado final é um medo intenso ou pânico. Isso é chamado de ciclo de pânico. Outro exemplo é pensar que a falta de ar significa que você está prestes a sufocar. Esse pensamento causará mais tensão física e mais sintomas de falta de ar, bem como tentativas de respirar mais profundamente, o que por sua vez pode contribuir para a falta de ar (por razões descritas posteriormente), e assim por diante.
 
 Em contraste, pensar que um coração acelerado é inofensivo e não motivo de preocupação interromperá esse ciclo negativo, com o resultado final de que o pânico não ocorre. Da mesma forma, perceber que a falta de ar não é um sinal de sufocamento iminente reduz as chances de um ataque de pânico.
 
-Pense em um ataque de pânico recente e nos sintomas físicos, pensamentos e comportamentos, e como eles contribuíram uns para os outros. O que aconteceu primeiro? Foi um sintoma físico ou um pensamento? O que aconteceu em seguida? Seus sintomas físicos aumentaram, e no que isso o levou a pensar? Como você se comportou? Como suas reações influenciaram seus sintomas físicos?"""]
-    
-}
+Pense em um ataque de pânico recente e nos sintomas físicos, pensamentos e comportamentos, e como eles contribuíram uns para os outros. O que aconteceu primeiro? Foi um sintoma físico ou um pensamento? O que aconteceu em seguida? Seus sintomas físicos aumentaram, e no que isso o levou a pensar? Como você se comportou? Como suas reações influenciaram seus sintomas físicos?"""],
+    'Sintomas físicos e o pânico': ["""Pensamentos negativos contribuem para o pânico, principalmente quando relacionados à crença de que os sintomas físicos são prejudiciais. Portanto, é essencial entender com precisão esses sintomas físicos para perceber que não são prejudiciais.
+
+Cientificamente, o medo imediato ou de curto prazo (ou seja, um ataque de pânico) é chamado de resposta de luta ou fuga. Seus efeitos visam a fugir do perigo ou, se isso falhar, lutar para sobreviver. O objetivo principal do pânico é nos proteger do perigo. Mesmo no mundo agitado de hoje, isso é relevante. Imagine-se atravessando uma rua quando um carro se aproxima rapidamente. Se você não sentisse medo, seria atingido. Na realidade, a resposta de luta ou fuga toma conta, e você sai do caminho. O objetivo do pânico é nos proteger, não nos prejudicar.
+
+Quando o perigo é detectado, o cérebro envia mensagens para o sistema nervoso autônomo, que tem dois ramos: o sistema nervoso simpático e o parassimpático. O sistema simpático é responsável por liberar energia e preparar o corpo para a ação (lutar ou fugir), enquanto o parassimpático restaura o estado normal. A ativação do sistema simpático é responsável pela maioria dos sintomas de ataque de pânico.
+
+O sistema simpático é um sistema tudo-ou-nada: quando é ativado, todas as suas partes respondem. Isso pode explicar por que a maioria dos ataques de pânico envolve muitos sintomas físicos, não apenas um ou dois. Além disso, o sistema simpático responde imediatamente, assim que o perigo está próximo. Isso é por que os sintomas físicos dos ataques de pânico podem ocorrer quase instantaneamente, em questão de segundos.
+
+O sistema simpático libera duas substâncias químicas, adrenalina e noradrenalina, das glândulas adrenais nos rins. Essas substâncias são usadas como mensageiros pelo sistema simpático para continuar a atividade, o que pode durar algum tempo. No entanto, a atividade do sistema simpático é interrompida de duas maneiras:
+
+As substâncias químicas adrenalina e noradrenalina são eventualmente destruídas por outras substâncias químicas no corpo.
+
+O sistema nervoso parassimpático, que geralmente tem efeitos opostos ao sistema simpático, é ativado e restaura o estado normal.
+
+Isso significa que o pânico não pode continuar para sempre nem atingir níveis cada vez mais prejudiciais. O sistema parassimpático impede o sistema simpático de se descontrolar.
+
+A adrenalina e a noradrenalina levam algum tempo para serem completamente destruídas. Mesmo depois que o sistema simpático parou de responder, você pode se sentir tenso por algum tempo, pois as substâncias ainda estão em seu sistema. Isso é perfeitamente normal e inofensivo. Na verdade, isso tem um propósito: na natureza, o perigo muitas vezes retorna. É útil para nós permanecermos tensos para que possamos reativar rapidamente a resposta de luta ou fuga se o perigo retornar.
+
+Cada efeito físico do sistema de luta ou fuga tem como objetivo nos preparar para lutar ou fugir, ou seja, nos proteger. Isso afeta nosso coração, fluxo sanguíneo, respiração, suor, pupilas, músculos e sistema digestivo, entre outras partes do corpo.
+
+Efeitos Cardiovasculares:
+
+A atividade no sistema simpático aumenta a frequência cardíaca e a força das batidas cardíacas. Isso melhora o fluxo sanguíneo, fornecendo mais oxigênio aos tecidos e removendo resíduos. Isso é vital para a preparação para a ação (lutar ou fugir), pois acelera o fluxo sanguíneo.
+
+O fluxo sanguíneo é redirecionado, afastando-o de onde não é necessário (por meio do estreitamento dos vasos sanguíneos) e direcionando-o para onde é mais necessário (por meio da expansão dos vasos sanguíneos). Por exemplo, o sangue é retirado da pele, dedos e dedos dos pés, para reduzir o risco de lesões. Em vez disso, o sangue vai para os grandes músculos, como as coxas, o coração e os bíceps, que precisam de oxigênio para lutar ou fugir.
+
+Essas mudanças físicas fazem o coração bater rápido e a pele ficar pálida e fria, principalmente ao redor das mãos e dos pés. Isso é uma reação normal em situações de medo ou ansiedade, indicando que o corpo está se preparando para agir.
+
+Efeitos na Respiração:
+
+A respiração fica mais rápida e profunda, pois o corpo precisa de mais oxigênio para lutar ou fugir. Às vezes, a respiração pode ficar desequilibrada e causar sintomas desconfortáveis, como falta de ar, sensação de sufocamento e dor ou aperto no peito.
+
+Além disso, o suprimento de sangue para a cabeça pode ser reduzido. Isso não é perigoso, mas pode causar sintomas desagradáveis, como tontura, visão turva, confusão, sensação de irrealidade e ondas de calor. Esses sintomas físicos podem ser desconfortáveis, mas não indicam algo seriamente errado com você.
+
+Efeitos nas Glândulas Sudoríparas:
+
+A resposta de luta ou fuga aumenta a transpiração para resfriar o corpo e permitir que você continue lutando ou fugindo sem sofrer com o calor. A transpiração é um sintoma comum de ansiedade e pânico.
+Outros Efeitos Físicos:
+
+As pupilas dos olhos se dilatam para permitir a entrada de mais luz, ajudando a escanear o ambiente em busca de perigo. Isso faz sentido, já que o pânico e a ansiedade são reações à percepção de ameaça.
+
+A boca fica seca devido à diminuição da salivação, e a atividade do sistema digestivo é reduzida para direcionar a energia para os músculos necessários para lutar ou fugir. Isso pode causar náuseas, sensação de peso no estômago e, às vezes, diarreia.
+
+Muitos grupos musculares se contraem em preparação para a luta ou fuga, resultando em tensão, dores, tremores e tremedeira.
+
+Outro efeito interessante é a liberação de analgésicos naturais pelo cérebro, reduzindo a sensação de dor quando estamos com medo. Isso nos permite continuar lutando ou fugindo, mesmo que estejamos feridos.
+
+Além disso, ocorre a liberação de coagulantes e linfócitos no sangue, que ajudam a fechar feridas e reparar danos nos tecidos. A velocidade de liberação de glóbulos vermelhos também aumenta para transportar mais oxigênio no sangue, e o fígado libera açúcar armazenado para fornecer mais energia aos músculos.
+
+Por fim, como a resposta de luta ou fuga ativa o corpo inteiro, o que requer muita energia, as pessoas geralmente se sentem cansadas e exaustas depois.
+
+Resumindo, as mudanças físicas que causam os sintomas físicos do pânico e da ansiedade são protetoras, destinadas a nos ajudar a escapar ou enfrentar o perigo. Os sintomas são reais, mas não prejudiciais. É importante entender que esses sintomas são uma resposta normal do corpo a situações de estresse e não indicam uma ameaça grave à saúde."""    
+]}
